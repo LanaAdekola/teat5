036521fb8f548015eb843aefa7e77d105ebf129a
LanaAdekola <adekolaadelana@gmail.com>
2023-01-16 05:02:44 -0800

    setting

---
+++ test 5.js
@@ -0,0 +1,7 @@
+const inputEl = document.getElementById("input-el")
+const inputBtn = document.getElementById("input-btn")
+const ulEl = document.getElementById("ul-el")
+
+inputBtn.addEventListener("click", function() {
+    myLeads.push(inputEl.value)
+    inputEl.value = ""
