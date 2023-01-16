7db8187e11c27ec7b8b8d23fb5d77aea8f360970
LanaAdekola <adekolaadelana@gmail.com>
2023-01-16 05:03:37 -0800

    Update test 5.js

--- test 5.js
+++ test 5.js
@@ -5,3 +5,17 @@ const ulEl = document.getElementById("ul-el")
 inputBtn.addEventListener("click", function() {
     myLeads.push(inputEl.value)
     inputEl.value = ""
+
+
+    function renderLeads() {
+    let listItems = ""
+    for (let i = 0; i < myLeads.length; i++) {
+        listItems += `
+            <li>
+                <a target='_blank' href='${myLeads[i]}'>
+                    ${myLeads[i]}
+                </a>
+            </li>
+        `
+    }
+    ulEl.innerHTML = listItems 
